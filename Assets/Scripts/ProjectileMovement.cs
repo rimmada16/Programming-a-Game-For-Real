88dcddde15128ad7bcb360864b8ec5d88486ba98
rimmada <60363927+rimmada16@users.noreply.github.com>
2023-11-16 17:03:38 +0000

    Hardcore mode that can be toggled

--- Assets/Scripts/ProjectileMovement.cs
+++ Assets/Scripts/ProjectileMovement.cs
@@ -7,19 +7,27 @@ using UnityEngine;
 public class ProjectileMovement : MonoBehaviour
 {
     [SerializeField] private int damage;
+    [SerializeField] private int hardcoreDamage;
     [SerializeField] private int knockback;
     public Transform source;
-
+    private GameObject HardcoreCheckerGO;
+    private int originalDamage;
+    
+    
     public float speed = 10f;
 
     // Projectile lifetime length -- Set to 1 second
     public float lifetime = 1;
     
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        // Will throw an error if it is not active - Tried implementing try catch but couldn't get it to work
+        HardcoreCheckerGO = GameObject.FindWithTag("HardcoreChecker");
+        
+        originalDamage = damage;
     }
+
 
     private void Awake()
     {
@@ -30,12 +38,25 @@ public class ProjectileMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-
+    
     {
         // Transforms the Projectile
         var transform1 = transform;
         transform1.position += transform1.forward * (Time.deltaTime * speed);
+        
+        // Hardcore
+        if (HardcoreCheckerGO.activeSelf)
+        {
+            Debug.Log("HARDCORE CHECKER IS ACTIVE");
+            damage = hardcoreDamage;
+        }
+        else
+        {
+            damage = originalDamage;
+        }
     }
+    
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == gameObject.layer)
