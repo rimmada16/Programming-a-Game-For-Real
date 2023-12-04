6e46a47725686b7060f9bb3896442be70a01ffeb
rimmada <60363927+rimmada16@users.noreply.github.com>
2023-12-04 00:59:50 +0000

    Gate UI + Flavour Text

--- Assets/Scripts/EnemyGateManager.cs
+++ Assets/Scripts/EnemyGateManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyGateManager : MonoBehaviour
 {
@@ -9,35 +12,58 @@ public class EnemyGateManager : MonoBehaviour
     // This needs to be stored in the Checkpoint system
     private int _enemiesKilled;
 
+    [SerializeField] private Text flavourText;
+    [SerializeField] private Canvas canvas;
+
     // Copy paste for each gate
     [Header("Health Gate One")]
     [SerializeField] private GameObject enemyGateOne;
     [SerializeField] private int enemyGateOneKillAmountRequired;
-    
-    void Awake()
+    [SerializeField] private TMP_Text enemyGateOneText;
+
+    private void Awake()
     {
         Instance = this;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.L))
         {
             Debug.Log(_enemiesKilled);
         }
-        
-        if (_enemiesKilled >= enemyGateOneKillAmountRequired)
+
+        // Gate one
+        if (enemyGateOne.activeSelf)
         {
-            enemyGateOne.SetActive(false);
-            // May add a text fade that says something like
-            // "A gate has opened somewhere..."
+            GateOne();
         }
     }
 
+    private void GateOne()
+    {
+        if (_enemiesKilled < enemyGateOneKillAmountRequired)
+        {
+            enemyGateOneText.SetText("Something blocks the way... <br>" + _enemiesKilled + "/" + enemyGateOneKillAmountRequired + "<br>Kills");
+        }
+
+        if (_enemiesKilled >= enemyGateOneKillAmountRequired)
+        {
+            enemyGateOne.SetActive(false);
+            FlavourText();
+        }
+    }
+
+    private void FlavourText()
+    {
+        var instantiatedText = Instantiate(flavourText, canvas.transform);
+        instantiatedText.text = "A gate has opened somewhere...";
+        Destroy(instantiatedText, 3f);
+    }
+    
     public void OnEnemyKilled()
     {
         _enemiesKilled++;
     }
-    
 }
